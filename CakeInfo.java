2ea8de6cc3b79b6766518501b04b50365d2997e2
ishhh02 <104405859+ishhh02@users.noreply.github.com>
2022-06-09 21:16:29 +1000

    Update CakeInfo.java

--- CakeInfo.java
+++ CakeInfo.java
@@ -21,46 +21,10 @@ public class CakeInfo {
 
     public void loadFlavours(String flavoursInfoFile) throws IOException {
         // Task 2.1: WRITE YOUR CODE HERE
-        try{
-            File cakeFlavours = new File(flavoursInfoFile);
-            Scanner myReader = new Scanner(cakeFlavours);
-            while(myReader.hasNextLine()){
-                String[]data= Scanner.nextLine()trim.split(":|\\");
-                for (int i = 0; i <flavoursInfo; ++i){
-                    for(int j = 0; j< data[i].length();j++){
-                        flavoursInfo [i][j] = data[i];
-                    }
-                }
-                System.out.println(data);
-            }catch(FileNotFoundException e){
-                System.out.println("An error occured.");
-                e.printStackTrace();
-            }
-            }
-        }
-    }
-
+        
     public void loadPriceInfo(String priceInfoFile) throws IOException {
         // Task 2.2: WRITE YOUR CODE HERE
-        try{
-            File cakePrice = new File(priceInfoFile);
-            Scanner myReader = new Scanner(cakePrice);
-            while(myReader.hasNextLine()){
-                String[]data = Scanner.nextLine()trim.split(":|\\");
-                for(int i =0; i< priceInfo; i++){
-                    for(int j = 0; j< data[i].length();j++){
-                        priceInfo = [i][j] = data[i];
-
-                    }
-                }
-                System.out.println(data);
-            }catch(FileNotFoundException e){
-                System.out.println("An error occured.");
-                e.printStackTrace();`
-            }
-        }
-
-    }
+        
     /*returns the cake price of a cake*/
     public float calCakePrice(Cake cake) { 
 	// Task 2.3: WRITE YOUR CODE HERE 
